b984a9cdf15ca3757eb45f2bbed45c6e33350213
YatokaiL <yatokirigami@gmail.com>
2025-10-14 20:01:06 +0300

    Инициализация проекта

---
+++ src/main/java/ru/prilepskij/routinerangerbot/RoutineRangerBotApplication.java
@@ -0,0 +1,13 @@
+package ru.prilepskij.routinerangerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineRangerBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoutineRangerBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/prilepskij/routinerangerbot/RoutineRangerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prilepskij.routinerangerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineRangerBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
